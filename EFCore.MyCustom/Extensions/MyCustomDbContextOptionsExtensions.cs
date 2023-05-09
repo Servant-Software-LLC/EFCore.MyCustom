3ab53af1bf45baa8b516569206095b583f3bcccc
David Maltby <david.maltby@hotmail.com>
2023-05-09 16:04:33 -0500

    Limit the type of DbConnection that is accepted for UseMyCustom() to only allow for SqlitConnection instances.

--- EFCore.MyCustom/Extensions/MyCustomDbContextOptionsExtensions.cs
+++ EFCore.MyCustom/Extensions/MyCustomDbContextOptionsExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Infrastructure;
 using EFCore.MyCustom.Infrastructure.Internal;
 using System.Data.Common;
+using Microsoft.Data.Sqlite;
 
 namespace Microsoft.EntityFrameworkCore;
 
@@ -18,7 +19,7 @@ public static class MyCustomDbContextOptionsExtensions
     }
 
     public static DbContextOptionsBuilder UseMyCustom(this DbContextOptionsBuilder optionsBuilder,
-                                                      DbConnection connection)
+                                                      SqliteConnection connection)
     {
         var extension = optionsBuilder.Options.FindExtension<MyCustomOptionsExtension>()
                          ?? new MyCustomOptionsExtension().WithConnection(connection);
