980de85b5400e15983bc2cd492ff9ef57d5712bf
David Maltby <david.maltby@hotmail.com>
2023-05-09 14:33:50 -0500

    Implementation of the Provider Specific Classes

---
+++ EFCore.MyCustom/Query/Internal/MyCustomQuerySqlGeneratorFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+
+namespace EFCore.MyCustom.Query.Internal;
+
+public class MyCustomQuerySqlGeneratorFactory : IQuerySqlGeneratorFactory
+{
+    public MyCustomQuerySqlGeneratorFactory(QuerySqlGeneratorDependencies dependencies)
+    {
+        Dependencies = dependencies;
+    }
+
+    /// <summary>
+    ///     Relational provider-specific dependencies for this service.
+    /// </summary>
+    protected virtual QuerySqlGeneratorDependencies Dependencies { get; }
+
+    public virtual QuerySqlGenerator Create()
+        => new MyCustomQuerySqlGenerator(Dependencies);
+}
